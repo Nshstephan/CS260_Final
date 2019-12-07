4febb53f58953c43dbce302f3fa09c9045d9f004
Nshstephan <nshstephan@gmail.com>
2019-12-07 23:44:34 +0400

    update

--- Stage_0/A09160432/Specification.java
+++ Stage_0/A09160432/Specification.java
@@ -106,3 +106,5 @@ public class Specification implements PlugInFilter {
     }
 
 }
+
+//copyright Narek Tumanyan
--- Stage_0/A09160432/SpecificationHSV.java
+++ Stage_0/A09160432/SpecificationHSV.java
@@ -136,3 +136,5 @@ public class SpecificationHSV implements PlugInFilter {
     }
 
 }
+
+//copyright Narek Tumanyan
